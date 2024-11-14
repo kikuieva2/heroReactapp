66cd6b6fee5663b2f0db1aca205176fd922b17e0
Karina Nikulisceva <nikulisceva.karina@inbox.lv>
2024-11-14 19:17:25 +0200

    Structure changed

--- latest/src/components/HeroDescription.jsx
+++ latest/src/components/HeroDescription.jsx
@@ -1,14 +1,14 @@
 import './HeroDescription.css';
 
-function HeroDescription(props){
+function HeroDescription({hero}){
     return(
         <div className="hero-body">
-        <h3 className="hero-title">{props.name}</h3>
-        <p className="hero-universe">{props.universe}</p>
-        <p className="hero-occupation">{props.occupation}</p>
-        <p className="hero-friends">{props.friends}</p>
-        <p className="hero-superpowers">{props.superpowers}</p>
-        <img src={props.url} alt={props.info} />
+        <h3 className="hero-title">{hero.name}</h3>
+        <p className="hero-universe">{hero.universe}</p>
+        <p className="hero-occupation">{hero.occupation}</p>
+        <p className="hero-friends">{hero.friends}</p>
+        <p className="hero-superpowers">{hero.superpowers}</p>
+        <img src={hero.url} alt={hero.info} />
         </div>
     )
 }
